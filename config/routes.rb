9a389bdcba4592406a1a0d5685ca5ca1ed5b2083
Ryo-2525 <seama.24.music@gmail.com>
2021-04-14 08:58:35 +0000

    いいね実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users,
   controllers: { registrations: 'registrations' }
-  
+
   root to: 'posts#index'
 
   resources :users, only: [:show]
 
   resources :posts, only: [:index, :new, :create, :show, :destroy] do
     resources :photos, only: [:create]
-    resources :likes, only: [:create, :destroy], shallow: true
-    resources :comments, only: [:index, :create, :destroy], shallow: true
+    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:index, :create, :destroy]
   end
 end
