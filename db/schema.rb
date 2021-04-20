12262c673d9ba931736af33975d4e4cb13504ec4
Ryo-2525 <seama.24.music@gmail.com>
2021-04-20 12:02:54 +0000

    db編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_050552) do
+ActiveRecord::Schema.define(version: 2021_04_15_080809) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -45,13 +45,6 @@ ActiveRecord::Schema.define(version: 2021_04_17_050552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
